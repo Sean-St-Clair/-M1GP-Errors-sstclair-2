ad610025226a0a883929d226f09bf29ccbfcd644
seans <sstclair@uvm.edu>
2023-01-25 20:14:07 -0800

    Included proper cout parameter when calling spie output methods

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     SPIE_Game game = *new SPIE_Game();
     game.print_rules(cout);
-    game.print_winning_numbers;
+    game.print_winning_numbers(cout);
 
     int score = 5;
     char choice = 'p';
